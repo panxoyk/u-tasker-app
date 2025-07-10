32130907775bcb6138181d8d52661724afc7a837
francisco.cstllhrnndz <francisco.cstllhrnndz@gmail.com>
2025-07-10 01:38:43 +0000

    Fix update full name onboarding page

--- src/actions/profile.ts
+++ src/actions/profile.ts
@@ -9,7 +9,11 @@ export const updateFullName = async ({
   last_name,
 }: UpdateFullNameFormData): Promise<ProfileAPIResponse> => {
   try {
-    const supabase = await createClient();
+    const supabase = await createClient();    
+    // Check if a user is logged in
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
     const { data: profile, error } = await supabase
       .from('profile')
@@ -17,14 +21,15 @@ export const updateFullName = async ({
         name: name,
         last_name: last_name,
       })
-      .select()
-      .single();
+      .eq('user_id', user?.id)
+      .select();
 
     if (error) {
       console.error('Error updating full name:', error);
       return { success: false, error: error.message };
     }
 
+    // @ts-ignore
     return { success: true, data: profile };
   } catch (e: any) {
     console.error('Unexpected error updating full name:', e);
